51130beef5a1f76afd3e5766f88b88e8758fd25a
Rajat Saxena <rajat.r.saxena@oracle.com>
2023-12-06 18:24:36 +0530

    [skip ci] first commit

---
+++ frontend/src/components/content/index.tsx
@@ -0,0 +1,9 @@
+import { h } from "preact";
+
+export function Content() {
+  return (
+    <div class="oj-web-applayout-max-width oj-web-applayout-content">
+      <p class="oj-typography-subheading-sm">This is the content area.</p>
+    </div>
+  );
+};
